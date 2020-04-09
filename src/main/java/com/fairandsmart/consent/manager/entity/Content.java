10a49da2816000fd95dd973d319e8c9e85cc2c82
jerome Blanchard <jayblanc@gmail.com>
2020-04-09 15:24:38 +0200

    Add first steps of a simple consent submit use case

--- src/main/java/com/fairandsmart/consent/manager/entity/Content.java
+++ src/main/java/com/fairandsmart/consent/manager/entity/Content.java
@@ -9,4 +9,30 @@ public class Content {
     public String body;
     public String footer;
 
+    public Content() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
+    public void setFooter(String footer) {
+        this.footer = footer;
+    }
 }
---
+++ src/test/java/com/fairandsmart/consent/usecase/SimpleCollectTest.java
@@ -0,0 +1,52 @@
+package com.fairandsmart.consent.usecase;
+
+import com.fairandsmart.consent.api.dto.CreateInformationDto;
+import com.fairandsmart.consent.manager.ConsentContext;
+import com.fairandsmart.consent.manager.entity.Information;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+@QuarkusTest
+public class SimpleCollectTest {
+
+    /**
+     * 1 : l'orga génère un context de collecte
+     *      le context contient le nom du user (subject) et les ids des traitements
+     * 2 : Le user appel une URL avec le context en paramètre (header ou query param),
+     *      cas 1 : subject inconnu pas de collecte précédente, on génère un formulaire vide avec les éléments demandés dans le context
+     * 3 : le user post le formulaire avec ses réponses sur une autre URL
+     *      cas 1 : tout est bon, un reçu est généré et renvoyé en réponse au user
+     * 4 : le user retourne à son url initiale qui devrait être contenu dans le reçu ou dans le context
+     */
+    @Test
+    public void testSimpleCollect() {
+        //Préconditions
+        CreateInformationDto h1 = new CreateInformationDto();
+        h1.setType(Information.Type.HEAD);
+        h1.setCountry("FR");
+        h1.setDefaultLanguage("fr");
+        h1.setName("H1");
+        h1.setDescription("Le header H1");
+        given().when().post("/models/informations", h1);
+
+
+        ConsentContext ctx = new ConsentContext()
+                .withSubject("mmichu")
+                .withOrientation(ConsentContext.Orientation.VERTICAL)
+                .withHeader("h1")
+                .withTreatments(Collections.singletonList("t1"))
+                .withFooter("f1");
+
+        given()
+            .when().post("/admin/token")
+            .then()
+            .statusCode(200)
+            .body(notNullValue());
+    }
+
+}
