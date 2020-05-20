f98ebce852448f0b09e89403271e1ead52eac73e
Frédéric Pierre <frederic.pierre@fairandsmart.com>
2020-05-20 15:41:08 +0200

    Add missing annotation @Provider

--- src/main/java/com/fairandsmart/consent/api/mapper/EntityNotFoundExceptionMapper.java
+++ src/main/java/com/fairandsmart/consent/api/mapper/EntityNotFoundExceptionMapper.java
@@ -6,7 +6,9 @@ import org.apache.http.HttpStatus;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
+@Provider
 public class EntityNotFoundExceptionMapper implements ExceptionMapper<EntityNotFoundException> {
 
     @Override
