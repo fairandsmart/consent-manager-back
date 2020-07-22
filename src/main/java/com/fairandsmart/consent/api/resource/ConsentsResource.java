7cc1156ef976670a83652975bed9b4e698bab83e
Manon Stremplewski <manon.stremplewski@fairandsmart.com>
2020-07-22 10:02:21 +0200

    PRD-966 : Affichage du code d'appel à l'API

--- src/main/java/com/fairandsmart/consent/api/resource/ConsentsResource.java
+++ src/main/java/com/fairandsmart/consent/api/resource/ConsentsResource.java
@@ -45,6 +45,23 @@ public class ConsentsResource {
         return consentService.buildToken(ctx);
     }
 
+    @POST
+    @Path("/bothtokens")
+    @RolesAllowed("admin")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, String> generateBothTokens(@Context SecurityContext sec, ConsentContext ctx) {
+        LOGGER.log(Level.INFO, "POST /consents/bothtokens");
+        ctx.setPreview(true);
+        String preview = consentService.buildToken(ctx);
+        ctx.setPreview(false);
+        String real = consentService.buildToken(ctx);
+        Map<String, String> result = new HashMap<>();
+        result.put("preview", preview);
+        result.put("real", real);
+        return result;
+    }
+
     @GET
     @Produces(MediaType.TEXT_HTML)
     public TemplateModel<ConsentForm> getForm(@HeaderParam("TOKEN") String htoken, @QueryParam("t") String qtoken) throws AccessDeniedException, TokenExpiredException, EntityNotFoundException, ConsentServiceException, InvalidTokenException {
