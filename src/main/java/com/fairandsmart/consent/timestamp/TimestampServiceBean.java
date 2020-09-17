ce89b9fbcb92dfcd8c19150e7b256de8c58151ba
Jerome Blanchard <jerome.blanchard@fairandsmart.com>
2020-09-17 16:24:11 +0200

    Small test about resource

--- src/main/java/com/fairandsmart/consent/timestamp/TimestampServiceBean.java
+++ src/main/java/com/fairandsmart/consent/timestamp/TimestampServiceBean.java
@@ -16,6 +16,7 @@ import xades4j.providers.impl.TSAHttpData;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.logging.Level;
@@ -36,8 +37,13 @@ public class TimestampServiceBean implements TimestampService {
     public void init() throws TimestampServiceException {
         LOGGER.log(Level.INFO, "Initializing TimestampService");
         try {
-            Path keystore = Paths.get(this.getClass().getClassLoader().getResource("keystore.jks").toURI());
-            KeyingDataProvider kp = new FileSystemKeyStoreKeyingDataProvider("JKS", keystore.toString(),
+            Path keystore1 = Paths.get(ClassLoader.getSystemClassLoader().getResource("keystore.jks").toURI());
+            LOGGER.log(Level.INFO, keystore1.toString());
+            LOGGER.log(Level.INFO, new File(keystore1.toString()).getAbsolutePath());
+            Path keystore2 = Paths.get(this.getClass().getClassLoader().getResource("keystore.jks").toURI());
+            LOGGER.log(Level.INFO, keystore2.toString());
+            LOGGER.log(Level.INFO, new File(keystore2.toString()).getAbsolutePath());
+            KeyingDataProvider kp = new FileSystemKeyStoreKeyingDataProvider("JKS", keystore1.toString(),
                     new FirstCertificateSelector(),
                     new DirectPasswordProvider("password"),
                     new DirectPasswordProvider("password"), false);
