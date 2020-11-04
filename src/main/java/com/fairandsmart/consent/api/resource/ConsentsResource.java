accedc33913ae3e47f4e371ea2cbcd6745887f8e
Jerome Blanchard <jerome.blanchard@fairandsmart.com>
2020-11-04 16:31:54 +0100

    Allow ConsentsResource form to be retrieve in JSON (no template rendering) Avoid retrieving token in header AND query but in query for html and in header for json

--- src/main/java/com/fairandsmart/consent/api/resource/ConsentsResource.java
+++ src/main/java/com/fairandsmart/consent/api/resource/ConsentsResource.java
@@ -13,6 +13,7 @@ import com.fairandsmart.consent.token.TokenServiceException;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import java.util.logging.Level;
@@ -43,38 +44,38 @@ public class ConsentsResource {
 
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public TemplateModel<ConsentForm> getForm(@HeaderParam("TOKEN") String htoken, @QueryParam("t") String qtoken) throws AccessDeniedException, TokenExpiredException, EntityNotFoundException, ConsentServiceException, InvalidTokenException, TemplateServiceException {
+    public TemplateModel<ConsentForm> getFormHtml(@QueryParam("t") @NotNull String token) throws TokenExpiredException, EntityNotFoundException, ConsentServiceException, InvalidTokenException, TemplateServiceException {
         LOGGER.log(Level.INFO, "GET /consents");
-        String token;
-        if (StringUtils.isNotEmpty(htoken)) {
-            token = htoken;
-        } else if (StringUtils.isNotEmpty(qtoken)) {
-            token = qtoken;
-        } else {
-            throw new AccessDeniedException("Unable to find token neither in header nor as query param");
-        }
         ConsentForm form = consentService.generateForm(token);
         return templateService.buildModel(form);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public ConsentForm getFormJson(@HeaderParam("TOKEN") @NotNull String token) throws TokenExpiredException, EntityNotFoundException, ConsentServiceException, InvalidTokenException {
+        LOGGER.log(Level.INFO, "GET /consents");
+        return consentService.generateForm(token);
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public TemplateModel<ConsentFormResult> postConsent(MultivaluedMap<String, String> values, @Context UriInfo uriInfo) throws AccessDeniedException, TokenExpiredException, InvalidTokenException, InvalidConsentException, ConsentServiceException, TokenServiceException, TemplateServiceException {
         LOGGER.log(Level.INFO, "POST /consents");
-        if (!values.containsKey("token")) {
+        if (values.containsKey("token")) {
+            ConsentTransaction tx = consentService.submitConsent(values.get("token").get(0), values);
+            UriBuilder uri = uriInfo.getBaseUriBuilder().path(ReceiptsResource.class).path(tx.getTransaction())
+                    .queryParam("t", tokenService.generateToken(tx));
+            ConsentContext ctx = (ConsentContext) tokenService.readToken(values.get("token").get(0));
+            ConsentFormResult templateData = new ConsentFormResult();
+            templateData.setContext(ctx);
+            if (ctx.getReceiptDisplayType() != null && ctx.getReceiptDisplayType() != ConsentContext.ReceiptDisplayType.NONE) {
+                uri.queryParam("format", ctx.getReceiptDisplayType());
+            }
+            templateData.setReceiptURI(uri.build());
+            return templateService.buildModel(templateData);
+        } else {
             throw new AccessDeniedException("unable to find token in form");
         }
-        ConsentTransaction tx = consentService.submitConsent(values.get("token").get(0), values);
-        UriBuilder uri = uriInfo.getBaseUriBuilder().path(ReceiptsResource.class).path(tx.getTransaction())
-                .queryParam("t", tokenService.generateToken(tx));
-        ConsentContext ctx = (ConsentContext) tokenService.readToken(values.get("token").get(0));
-        ConsentFormResult templateData = new ConsentFormResult();
-        templateData.setContext(ctx);
-        if (ctx.getReceiptDisplayType() != null && ctx.getReceiptDisplayType() != ConsentContext.ReceiptDisplayType.NONE) {
-                uri.queryParam("format", ctx.getReceiptDisplayType());
-        }
-        templateData.setReceiptURI(uri.build());
-        return templateService.buildModel(templateData);
     }
 
 }
