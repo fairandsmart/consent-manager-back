641071dcf38d012d5d3e22ee77a8cb72f0b93133
Manon Stremplewski <manon.stremplewski@fairandsmart.com>
2021-01-15 08:07:07 +0100

    PRD-1281: Fixed RecordsResource endpoints annotations + added missing license doc to some files

--- src/main/java/com/fairandsmart/consent/api/resource/RecordsResource.java
+++ src/main/java/com/fairandsmart/consent/api/resource/RecordsResource.java
@@ -1,5 +1,38 @@
 package com.fairandsmart.consent.api.resource;
 
+/*-
+ * #%L
+ * Right Consent / A Consent Manager Platform
+ *
+ * Authors:
+ *
+ * Xavier Lefevre <xavier.lefevre@fairandsmart.com> / FairAndSmart
+ * Nicolas Rueff <nicolas.rueff@fairandsmart.com> / FairAndSmart
+ * Jérôme Blanchard <jerome.blanchard@fairandsmart.com> / FairAndSmart
+ * Alan Balbo <alan.balbo@fairandsmart.com> / FairAndSmart
+ * Frederic Pierre <frederic.pierre@fairansmart.com> / FairAndSmart
+ * Victor Guillaume <victor.guillaume@fairandsmart.com> / FairAndSmart
+ * Manon Stremplewski <manon.stremplewski@fairandsmart.com> / FairAndSmart
+ * Pauline Kullmann <pauline.kullmmann@fairandsmart.com> / FairAndSmart
+ * %%
+ * Copyright (C) 2020 Fair And Smart
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import com.fairandsmart.consent.api.dto.ExtractionConfigDto;
 import com.fairandsmart.consent.api.dto.ExtractionResultDto;
 import com.fairandsmart.consent.api.dto.RecordDto;
@@ -14,7 +47,7 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -55,7 +88,7 @@ public class RecordsResource {
             @APIResponse( responseCode = "401", description = "Access denied (you must be either the subject or an operator)"),
             @APIResponse( responseCode = "200", description = "A List of all Records that matches the extraction config") })
     @Operation( operationId = "extractRecords", summary = "Extract records according to the specific provided config")
-    public List<ExtractionResultDto> extractRecords(@NotEmpty @Valid ExtractionConfigDto dto) throws AccessDeniedException {
+    public List<ExtractionResultDto> extractRecords(@NotNull @Valid ExtractionConfigDto dto) throws AccessDeniedException {
         LOGGER.log(Level.INFO, "POST /records/extraction");
         return consentService.extractRecords(dto.getCondition().getKey(), dto.getCondition().getValue(), dto.getCondition().isRegexpValue()).entrySet().stream().map(ExtractionResultDto::build).collect(Collectors.toList());
     }
@@ -68,7 +101,7 @@ public class RecordsResource {
             @APIResponse( responseCode = "401", description = "Access denied (you must be either the subject or an operator)"),
             @APIResponse( responseCode = "200", description = "A List of all Records that matches the extraction config") })
     @Operation( operationId = "extractRecords", summary = "Extract records according to the specific provided config")
-    public List<ExtractionResultDto> extractRecordsCsv(@NotEmpty @Valid ExtractionConfigDto dto) throws AccessDeniedException {
+    public List<ExtractionResultDto> extractRecordsCsv(@NotNull @Valid ExtractionConfigDto dto) throws AccessDeniedException {
         LOGGER.log(Level.INFO, "POST /records/extraction");
         return consentService.extractRecords(dto.getCondition().getKey(), dto.getCondition().getValue(), dto.getCondition().isRegexpValue()).entrySet().stream().map(ExtractionResultDto::build).collect(Collectors.toList());
     }
