44bad6955a1d71643d1634f9c141b6aa6c295e66
Nicolas Rueff <nicolas@enere.fr>
2021-01-20 09:26:56 +0100

    Fixed quarkus conf in dev mode

---
+++ src/main/java/com/fairandsmart/consent/common/consts/OpenAPI.java
@@ -0,0 +1,11 @@
+package com.fairandsmart.consent.common.consts;
+
+public class OpenAPI {
+    public static final String TS_2021_01_01 = "1609459200000";
+    public static final String NIL_UUID ="00000000-0000-0000-0000-000000000000";
+    public static final String NOBODY_AT_EXAMPLE_COM = "nobody@example.com";
+    public static final String A_MYSTERIOUS_CUSTOMER = "a mysterious customer";
+
+    void OpenApi() {
+    }
+}
