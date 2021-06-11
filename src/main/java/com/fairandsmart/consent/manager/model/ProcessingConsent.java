b0fb5059fd01fde2fe8dde8de0818404ac7244da
Manon Stremplewski <manon.stremplewski@fairandsmart.com>
2021-06-11 08:47:58 +0200

    Added missing license headers

--- src/main/java/com/fairandsmart/consent/manager/model/ProcessingConsent.java
+++ src/main/java/com/fairandsmart/consent/manager/model/ProcessingConsent.java
@@ -1,5 +1,21 @@
 package com.fairandsmart.consent.manager.model;
 
+/*-
+ * #%L
+ * Right Consents, a Universal Consents & Preferences Management Platform.
+ * %%
+ * Copyright (C) 2020 - 2021 Fair And Smart
+ * %%
+ * This file is part of Right Consents Community Edition.
+ *
+ * Right Consents Community Edition is published by FAIR AND SMART under the
+ * GNU GENERAL PUBLIC LICENCE Version 3 (GPLv3) and a set of additional terms.
+ *
+ * For more information, please see the “LICENSE” and “LICENSE.FAIRANDSMART”
+ * files, or see https://www.fairandsmart.com/opensource/.
+ * #L%
+ */
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
